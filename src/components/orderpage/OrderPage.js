bf86a4848f536d6ecd0e6a7c78cc3e1dc5c6ded1
Heimdall Sham <heim.sham@icloud.com>
2021-09-17 11:13:55 -0400

    Updated partials. Created calendar to be passed off to Violetta for animation.

--- src/components/orderpage/OrderPage.js
+++ src/components/orderpage/OrderPage.js
@@ -4,26 +4,31 @@ import './OrderPage.scss';
 function OrderPage() {
     return (
         <section className="order__week">
-            <div className="orer__date-container">
+            <div className="order__date-container">
                 <div className="order__day">Mon</div>
                 <div className="order__day">20</div>
             </div>
             <div className="order__date-container">
                 <div className="order__day">Tues</div>
                 <div className="order__day">21</div>
-            </div><div className="order__date-container">
+            </div>
+            <div className="order__date-container">
                 <div className="order__day">Wed</div>
                 <div className="order__day">22</div>
-            </div><div className="order__date-container">
+            </div>
+            <div className="order__date-container">
                 <div className="order__day">Thurs</div>
                 <div className="order__day">23</div>
-            </div><div className="order__date-container">
+            </div>
+            <div className="order__date-container">
                 <div className="order__day">Fri</div>
                 <div className="order__day">24</div>
-            </div><div className="order__date-container">
+            </div>
+            <div className="order__date-container">
                 <div className="order__day">Sat</div>
                 <div className="order__day">25</div>
-            </div><div className="order__date-container">
+            </div>
+            <div className="order__date-container">
                 <div className="order__day">Sun</div>
                 <div className="order__day">26</div>
             </div>
