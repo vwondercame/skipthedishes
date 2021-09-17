312900dceedb57db96760ac298f6f01b6e79f9f3
Heimdall Sham <heim.sham@icloud.com>
2021-09-17 13:10:32 -0400

    Completed functionality on Wednesday schedule pop up.

--- src/components/orderpage/OrderPage.js
+++ src/components/orderpage/OrderPage.js
@@ -36,7 +36,7 @@ class OrderPage extends Component {
                         <div className="order__day">Tues</div>
                         <div className="order__day">21</div>
                     </div>
-                    <div className="order__date-container">
+                    <div className="order__date-container" onClick = {() => this.showSchedule()}>
                         <div className="order__day">Wed</div>
                         <div className="order__day">22</div>
                     </div>
@@ -57,46 +57,49 @@ class OrderPage extends Component {
                         <div className="order__day">26</div>
                     </div>
                 </section>
-                <section className="order__schedule">
-                    <h2>Wednesday</h2>
-                    <p className="order__select-text">Select Delivery Time:</p>
-                    <div className="order__time-container">
-                        <select className="order__time order__time--hour" name="timer">
-                            <option>---</option>
-                            <option>12:00</option>
-                            <option>12:30</option>
-                            <option>1:00</option>
-                            <option>1:30</option>
-                            <option>2:00</option>
-                            <option>2:30</option>
-                            <option>3:00</option>
-                            <option>3:30</option>
-                            <option>4:00</option>
-                            <option>4:30</option>
-                            <option>5:00</option>
-                            <option>5:30</option>
-                            <option>6:00</option>
-                            <option>6:30</option>
-                            <option>7:00</option>
-                            <option>7:30</option>
-                            <option>8:00</option>
-                            <option>8:30</option>
-                            <option>9:00</option>
-                            <option>9:30</option>
-                            <option>10:00</option>
-                            <option>10:30</option>
-                            <option>11:00</option>
-                            <option>11:30</option>
-                        </select>
-                        <select name="hour" className="order__time order__time--ap">
-                            <option>---</option>
-                            <option>AM</option>
-                            <option>PM</option>
-                        </select>
-                        <p className="order__discount">-10%</p>
-                        <button className="order__button">ADD TO DAY</button>
-                    </div>
-                </section>
+                {this.state.displaySchedule &&
+                <>
+                    <section className="order__schedule"/>
+                        <h2>Wednesday</h2>
+                        <p className="order__select-text">Select Delivery Time:</p>
+                        <div className="order__time-container">
+                            <select className="order__time order__time--hour" name="timer">
+                                <option>---</option>
+                                <option>12:00</option>
+                                <option>12:30</option>
+                                <option>1:00</option>
+                                <option>1:30</option>
+                                <option>2:00</option>
+                                <option>2:30</option>
+                                <option>3:00</option>
+                                <option>3:30</option>
+                                <option>4:00</option>
+                                <option>4:30</option>
+                                <option>5:00</option>
+                                <option>5:30</option>
+                                <option>6:00</option>
+                                <option>6:30</option>
+                                <option>7:00</option>
+                                <option>7:30</option>
+                                <option>8:00</option>
+                                <option>8:30</option>
+                                <option>9:00</option>
+                                <option>9:30</option>
+                                <option>10:00</option>
+                                <option>10:30</option>
+                                <option>11:00</option>
+                                <option>11:30</option>
+                            </select>
+                            <select name="hour" className="order__time order__time--ap">
+                                <option>---</option>
+                                <option>AM</option>
+                                <option>PM</option>
+                            </select>
+                            <p className="order__discount">-10%</p>
+                            <button className="order__button">ADD TO DAY</button>
+                        </div>
+                </>
+                }
             </div>
             </>
         )
